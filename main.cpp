3f7608d9e47d523b4d2919a7821bb1838b7c0d44
marcus2809 <marcusvinicius.paz2001@gmail.com>
2021-08-18 17:28:05 -0300

    Interface inicial

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+
+int main ()
+{
+    DicioAVL<int,char> D; int i;
+
+    for (i = 48; i < 58; ++i) if (D.inserir(i, (char) i) == D.fim()) return 1;
+
+    for (auto it = D.inicio(); it != D.fim(); ++it) {
+        cout << "O código de ’" << it.valor() << "’ é " << it.chave() << ’\n’;
+    }
+    for (i = 48; i < 58; ++i) {
+        auto it = D.buscar(i);
+    }
+}
