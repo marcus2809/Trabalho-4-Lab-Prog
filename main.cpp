736239f56054bb8367125210499383953030d2b5
marcus2809 <marcusvinicius.paz2001@gmail.com>
2021-08-18 17:34:47 -0300

    classe DicioAVL inserida

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using std::cout;
 
+template <typename T1, typename T2> 
+class DicioAVL{};
+
 int main ()
 {
     DicioAVL<int,char> D; int i;
@@ -8,7 +11,7 @@ int main ()
     for (i = 48; i < 58; ++i) if (D.inserir(i, (char) i) == D.fim()) return 1;
 
     for (auto it = D.inicio(); it != D.fim(); ++it) {
-        cout << "O código de ’" << it.valor() << "’ é " << it.chave() << ’\n’;
+        cout << "O código de ’" << it.valor() << "’ é " << it.chave() << '\n';
     }
     for (i = 48; i < 58; ++i) {
         auto it = D.buscar(i);
